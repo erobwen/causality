ec0686f570107c801abf98174c18ccb95d3ec53d
Robert Renbris <robert.renbris@prek.se>
2016-11-09 00:05:48 +0100

    Improved get map

--- causality.js
+++ causality.js
@@ -41,19 +41,35 @@
 
 
     // Helper to quickly get a child object
-    function getMap(object, key) {
-        if (typeof(object[key]) === 'undefined') {
-            object[key] = {};
+    function getMap() {
+        var argumentList = argumentsToArray(arguments);
+        var object = argumentList.shift();
+        while (argumentList.length > 0) {
+            var key = argumentList.shift();
+            if (typeof(object[key]) === 'undefined') {
+                object[key] = {};
+            }
+            object = object[key];
         }
-        return object[key];
+        return object;
     }
 
     // Helper to quickly get a child array
-    function getArray(object, key) {
-        if (typeof(object[key]) === 'undefined') {
-            object[key] = [];
+    function getArray() {
+        var argumentList = argumentsToArray(arguments);
+        var object = argumentList.shift();
+        while (argumentList.length > 0) {
+            var key = argumentList.shift();
+            if (typeof(object[key]) === 'undefined') {
+                if (argumentList.length === 0) {
+                    object[key] = [];
+                } else {
+                    object[key] = {};
+                }
+            }
+            object = object[key];
         }
-        return object[key];
+        return object;
     }
 
     function startsWith(prefix, string) {
