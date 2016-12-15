0b7711484ec3824cc155f149004997c2fbc6b32d
Robert Renbris <robert.renbris@prek.se>
2016-12-15 21:39:51 +0100

    test no side effects array

--- test/withoutSideEffects.js
+++ test/withoutSideEffects.js
@@ -6,16 +6,19 @@ describe("Without side effects", function(){
     it("Test no side effects", function(){
 
         let x = create({});
+        let x2 = create([]);
         let returnValue = withoutSideEffects(function() {
             y = create({});
             y.v = 42;
 
             x.v = 42;
+            x2.push(42);
 
             return y.v;
         });
 
         assert.equal(typeof(x.v), 'undefined');
         assert.equal(returnValue, 42);
+        assert.equal(x2.length, 0);
     });
 });
