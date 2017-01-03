d39ecc07b65f00aef05204a5ab14853068cbb76c
Jonas Liljegren <jonas@avisita.com>
2017-01-03 19:01:02 +0100

    Added some tests for observed class objects

---
+++ test/classes.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+require('../causality').install();
+
+const log = console.log.bind(console);
+
+describe("Classes", function () {
+
+    class MyCausalityClass {
+	    constructor( myid ){
+		    this.propA = myid;
+            this.propB = 0;
+            return create( this );
+        }
+
+        dostuff()
+        {
+            return 33;
+        }
+    }
+    
+    let cnt = 0;
+    const x = new MyCausalityClass( 11 );
+    let z;
+
+    repeatOnChange(function () {
+        cnt++;
+        z = x.propA + x.dostuff();
+    });
+
+    it('does action', function () {
+        assert.equal(z, 44);
+    });
+
+    it('not trigger', function () {
+        x.propC = 100;
+        assert.equal(cnt, 1);
+    });
+
+    it('trigger', function () {
+        x.propA = 2;
+        assert.equal(z, 35);
+    });
+
+    it('changed method does not trigger', function(){
+        MyCausalityClass.prototype.dostuff = function(){ return 11 };
+        assert.equal(z, 35);
+    });
+
+    it('changed object functions do trigger', function(){
+        x.dostuff = function(){ return 55 };
+        assert.equal(z, 57);
+    });
+
+
+});
+    
