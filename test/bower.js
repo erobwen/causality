6c19c26178263b83723287c16d3ae208981163a7
Jonas Liljegren <jonas@avisita.com>
2017-01-10 16:44:06 +0100

    Testing bower file

---
+++ test/bower.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const log = console.log.bind(console);
+const fs = require('fs');
+
+it('validate', function(){
+
+    const data = fs.readFileSync('bower.json');
+    const json = JSON.parse(data);
+    const bj = require('bower-json');
+    const norm = bj.parse(json);
+
+    assert.equal(json.main, "causality.js");
+});
