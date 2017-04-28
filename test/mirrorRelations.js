75051384acd0c6fdb4921dd78815d9205883d422
Robert Renbris <robert.wensman@gmail.com>
2017-04-28 19:58:43 +0200

    Tidy up testcase.

--- test/mirrorRelations.js
+++ test/mirrorRelations.js
@@ -6,24 +6,16 @@ describe("Mirror Relations", function(){
     it("Testing mirror relation exists", function(){
 		let x = create({_mirror_is_reflected : true});
 		let y = create({_mirror_reflects : true});
-		// console.log(x.__id);
-		// console.log(y.__id);
+		
+		// Assign x.foo
 		x.foo = y;
-		console.log(" ====  After assign ==== "); 
-		logPattern(x.__id);
-		logPattern(x, { foo : {}});
-		logPattern(y.__id);
-		logPattern(y, { _mirror_incoming_relations : { foo : { contents: {}}}});
-		console.log(" ====  All incoming ==== "); 
+
+		// Analyze incoming structure
+		let yIncomingFoo = []
 		mirror.forAllIncoming(y, 'foo', function(referer) {
-			console.log(" ====  x ==== "); 
-			logPattern(x.__id);
-			logPattern(x, { foo : {}});
-			console.log(" ====  referer ==== "); 
-			logPattern(referer.__id);
-			logPattern(referer, { foo : { contents: { "1": {}}}});
-			console.log(" ====  equals ==== "); 
-			console.log(x === referer);
+			yIncomingFoo.push(referer);
 		});
+		
+		assert.equal(yIncomingFoo[0], x);
     });
 });
