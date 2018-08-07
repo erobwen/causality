c11e7749876bbe59ecd0b801df12f1f35a5f5232
Jonas Liljegren <jonas@avisita.com>
2018-08-07 14:08:27 +0200

    package cleanup

--- experiments/async/expected.js
+++ experiments/async/expected.js
@@ -12,11 +12,11 @@ async function renderExpected(){
         for( let a of await l.listAP() ){
             log( '    A', a.obs.id );
             
-            const r0 = (await a.listRP())[0];
-            log( '    R0', r0.obs.id );
+            const r = [... await a.listRP() ];
+            log( '    R0', r[0].obs.id );
             
             for( let bd of await b.listDP() ){
-                const ad = await a.listDP();
+                const ad = [... await a.listDP() ];
                 const inc = ad.map( d => d.obs.id ).includes( bd.obs.id );
                 log( '      D', inc?'+':'-', bd.obs.id );
             }
