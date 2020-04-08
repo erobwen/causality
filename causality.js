a1f8af3494a1636da93b1655027820bfe2135d6d
erobwen <robert.wensman@gmail.com>
2020-04-08 20:55:15 +0200

    ...

--- causality.js
+++ causality.js
@@ -1,6 +1,8 @@
 'use strict'; 
 // require = require("esm")(module);
 const { argumentsToArray, configSignature, mergeInto } = require("./lib/utility.js");
+const { defaultDependencyInterfaceCreator } = require("./lib/defaultDependencyInterface.js");
+
 // const log = console.log;
 // const logg = (string) => {
 //   console.log("-------------" + string + "-------------");
@@ -26,7 +28,7 @@ function createInstance(configuration) {
     onEventGlobal = null,
     emitReCreationEvents = false,
     
-    customDependencyRecorder = null, //{recordDependencyOnArray, recordDependencyOnEnumeration, recordDependencyOnProperty, recordDependency}
+    customDependencyInterfaceCreator = null, //{recordDependencyOnArray, recordDependencyOnEnumeration, recordDependencyOnProperty, recordDependency}
     customCreateInvalidator = null, 
     customCreateRepeater = null,
 
@@ -708,113 +710,16 @@ function createInstance(configuration) {
    *
    **********************************/
 
-  function withoutRecording(action) {
-    state.invalidatorPaused++;
-    updateContextState();
-    action();
-    state.invalidatorPaused--;
-    updateContextState();
-  }
 
-  const recordDependencyOnArray = customDependencyRecorder ? customDependencyRecorder.recordDependencyOnArray : defaultRecordDependencyOnArray;
-  const recordDependencyOnEnumeration = customDependencyRecorder ? customDependencyRecorder.recordDependencyOnEnumeration : defaultRecordDependencyOnEnumeration;
-  const recordDependencyOnProperty = customDependencyRecorder ? customDependencyRecorder.recordDependencyOnProperty : defaultRecordDependencyOnProperty;
-  const recordDependency = customDependencyRecorder ? customDependencyRecorder.recordDependency : defaultRecordDependency;
+  const dependencyInterface = customDependencyInterfaceCreator ? 
+    customDependencyInterfaceCreator(invalidateObserver) 
+    : 
+    defaultDependencyInterfaceCreator(invalidateObserver);
 
-  function defaultRecordDependencyOnArray(recorder, handler) {
-    if (handler._arrayObservers === null) {
-      handler._arrayObservers = {};
-    }
-    recordDependency(recorder, "_arrayObservers", handler._arrayObservers);//object
-  }
+  const recordDependencyOnArray = dependencyInterface.recordDependencyOnArray;
+  const recordDependencyOnEnumeration = dependencyInterface.recordDependencyOnEnumeration;
+  const recordDependencyOnProperty = dependencyInterface.recordDependencyOnProperty;
 
-  function defaultRecordDependencyOnEnumeration(recorder, handler) {
-    if (typeof(handler._enumerateObservers) === 'undefined') {
-      handler._enumerateObservers = {};
-    }
-    recordDependency(recorder, "_enumerateObservers", handler._enumerateObservers);
-  }
-
-  function defaultRecordDependencyOnProperty(recorder, handler, key) {    
-    if (typeof(handler._propertyObservers) ===  'undefined') {
-      handler._propertyObservers = {};
-    }
-    if (typeof(handler._propertyObservers[key]) ===  'undefined') {
-      handler._propertyObservers[key] = {};
-    }
-    recordDependency(recorder, "_propertyObservers." + key, handler._propertyObservers[key]);
-  }
-
-  let sourcesObserverSetChunkSize = 500;
-  function defaultRecordDependency(recorder, description, observerSet) {
-    if (typeof(recorder) === "string") {
-      observerSet = description;
-      description = recorder; 
-      recorder = activeRecorder;
-
-    }
-    // instance can be a cached method if observing its return value,
-    // object & definition only needed for debugging.
-
-    if (recorder !== null) {
-      if (typeof(observerSet.initialized) === 'undefined') {
-        observerSet.description = description;
-        observerSet.isRoot = true;
-        observerSet.contents = {};
-        observerSet.contentsCounter = 0;
-        observerSet.initialized = true;
-        observerSet.first = null;
-        observerSet.last = null;
-      }
-
-      let recorderId = recorder.id;
-
-      if (typeof(observerSet.contents[recorderId]) !== 'undefined') {
-        return;
-      }
-
-      if (observerSet.contentsCounter === sourcesObserverSetChunkSize &&
-          observerSet.last !== null) {
-        observerSet = observerSet.last;
-        if (typeof(observerSet.contents[recorderId]) !== 'undefined') {
-          return;
-        }
-      }
-      if (observerSet.contentsCounter === sourcesObserverSetChunkSize) {
-        let newChunk =
-          {
-            isRoot : false,
-            contents: {},
-            contentsCounter: 0,
-            next: null,
-            previous: null,
-            parent: null
-          };
-        if (observerSet.isRoot) {
-          newChunk.parent = observerSet;
-          observerSet.first = newChunk;
-          observerSet.last = newChunk;
-        } else {
-          observerSet.next = newChunk;
-          newChunk.previous = observerSet;
-          newChunk.parent = observerSet.parent;
-          observerSet.parent.last = newChunk;
-        }
-        observerSet = newChunk;
-      }
-
-      // Add repeater on object beeing observed,
-      // if not already added before
-      let observerSetContents = observerSet.contents;
-      if (typeof(observerSetContents[recorderId]) === 'undefined') {
-        observerSet.contentsCounter = observerSet.contentsCounter + 1;
-        observerSetContents[recorderId] = recorder;
-
-        // Note dependency in repeater itself (for cleaning up)
-        recorder.sources.push(observerSet);
-      }
-    }
-  }
 
   function invalidateArrayObservers(handler) {  
     if (handler._arrayObservers !== null) {
@@ -926,9 +831,20 @@ function createInstance(configuration) {
   }
 
 
+
   /** -------------
    *  Upon change
    * -------------- */
+
+
+  function withoutRecording(action) {
+    state.invalidatorPaused++;
+    updateContextState();
+    action();
+    state.invalidatorPaused--;
+    updateContextState();
+  }
+
 
   let nextObserverToInvalidate = null;
   let lastObserverToInvalidate = null;
@@ -1301,7 +1217,6 @@ function createInstance(configuration) {
     state,
     enterContext,
     leaveContext,
-    recordDependency,
     invalidateObservers
   }); 
 
