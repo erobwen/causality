a0f1fd79af6998569a15d9387b589d4e989990e8
Robert Renbris <robert.renbris@gmail.com>
2022-01-28 22:15:50 +0100

    ...

--- test/valueTypes.js
+++ test/valueTypes.js
@@ -13,8 +13,14 @@ describe("Value types", function(){
 
     // Main object
     // Note: Here it is assumed that observable will not recusivley create observables out of the whole structure. 
-    const foo = observable({value: 1, reference: other, complexValue: {bar: 32, fum: 42}});  
+    const foo = observable({
+      value: 1, 
+      reference: other, 
+      complexValue: {bar: 32, fum: 42}
+    });
+      
     let repeatCounter = 0;
+    let expectedRepeatCounter = 0;
 
     repeat(() => {
       // Just observe it: 
@@ -23,22 +29,22 @@ describe("Value types", function(){
       const c = foo.complexValue; 
       repeatCounter++;
     })
-    assert.equal(repeatCounter, 1);
+    assert.equal(repeatCounter, ++expectedRepeatCounter); 
     
     // Changes
     foo.value = 1; // Should not trigger reactions (there is no assignemnt as it is the same as old value)
-    assert.equal(repeatCounter, 1);
+    assert.equal(repeatCounter, expectedRepeatCounter);
     
     foo.reference = other; // Will will not trigger reaction 
-    assert.equal(repeatCounter, 1);
+    assert.equal(repeatCounter, expectedRepeatCounter);
     
     foo.reference = another; // This will trigger reaction 
-    assert.equal(repeatCounter, 2);
+    assert.equal(repeatCounter, ++expectedRepeatCounter);
     
     foo.complexValue = {bar: 32, fum: 42} // Should also not trigger reaction!
-    assert.equal(repeatCounter, 2);
+    assert.equal(repeatCounter, expectedRepeatCounter);
 
     foo.complexValue = {bar: "x", fum: 43} // But this should!
-    assert.equal(repeatCounter, 3);
+    assert.equal(repeatCounter, ++expectedRepeatCounter);
   });
 });
