f931361eed2458a18d815d4b987696e6ca10fe74
Jonas Liljegren <jonas@avisita.com>
2022-05-11 16:27:51 +0200

    Building more tests

---
+++ test/transaction.js
@@ -0,0 +1,68 @@
+require = require("esm")(module);
+const {create,repeat,state,transaction} = require("../causality.js").getWorld();
+const assert = require('assert');
+
+describe( "transactions", ()=>{
+
+  const x = create({A: 11});
+  const y = create({A: 21});
+
+  console.log(`x(${x.causality.id})`);
+  console.log(`y(${y.causality.id})`);
+  
+  repeat( "Rx", (repeater) =>{
+
+    console.log("repeat", repeater.id, repeater.description);
+    if( x.A < 13 ) y.A = x.A;
+    console.log( repeater.sourcesString() );
+  });
+  
+  repeat( "Ry", (repeater) =>{
+    console.log("repeat", repeater.id, repeater.description );
+    y.B = y.A - 10;
+    console.log( repeater.sourcesString() );
+  });
+  
+  
+  //console.log("state", state);
+  
+  
+  it("nested reacts during non-transaction", ()=>{
+
+//    state.afterRepeatersRefreshed = ()=>{
+//      assert.equal( x.A, 12 );
+//      assert.equal( y.B, 2 );
+//      state.afterRepeatersRefreshed = null;
+//      done();
+//    }
+    
+    x.A ++;
+
+    assert.equal( x.A, 12 );
+    assert.equal( y.B, 2 );
+    
+  });
+
+  it("postpone reactions in transaction", ()=>{
+
+
+//    state.afterRepeatersRefreshed = ()=>{
+//      console.log( "after transaction", {x,y, state} );
+//      //assert.equal( x.A, 12 );
+//      //assert.equal( y.B, 2 );
+//      state.afterRepeatersRefreshed = null;
+//      done();
+//    }
+
+    transaction(()=>{
+      x.A ++;
+      //console.log( "in transaction", {x,y, state} );
+    });
+    
+    console.log( "after transaction", {x,y, state} );
+    assert.equal( y.B, 3 );
+
+  });
+  
+  
+});
